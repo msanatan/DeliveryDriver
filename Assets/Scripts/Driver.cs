a71c6f7d8856c22e4498f68daec2d906774d56fc
Marcus Sanatan <msanatan@gmail.com>
2022-09-24 11:57:43 -0400

    Use new input system's actions to move car
    A lot easier to do than the C# delegate tutorials showed me.

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Driver : MonoBehaviour
 {
+    // [SerializeField] InputManager inputManager;
+    [SerializeField] float steerSpeed = 1f;
+    [SerializeField] float moveSpeed = 1f;
+    Vector3 movement;
+    Vector3 rotation;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+    }
+
+    void OnMove(InputValue value)
+    {
+        var moveInput = value.Get<Vector2>();
+        movement.y = moveInput.y;
+        rotation.z = moveInput.x * -steerSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(0, this.movement.y * moveSpeed * Time.deltaTime, 0);
+        transform.Rotate(this.rotation * Time.deltaTime);
     }
 }
