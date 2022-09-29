2ed47e6db6733c08bcf339155977fa889230405a
Marcus Sanatan <msanatan@gmail.com>
2022-09-29 07:09:59 -0400

    Change colour of car when a package is collected

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    [SerializeField] Color32 hasPackageColour = new Color32(8, 255, 0, 255);
+    [SerializeField] Color32 noPackageColour = new Color32(255, 255, 255, 255);
     [SerializeField] float packageDestroyDelay = 0.5f;
     bool hasPackage;
+    SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     void OnCollisionEnter2D(Collision2D other)
     {
@@ -18,12 +26,14 @@ public class Delivery : MonoBehaviour
         {
             Debug.Log("Package picked up");
             hasPackage = true;
+            spriteRenderer.color = hasPackageColour;
             Destroy(other.gameObject, packageDestroyDelay);
         }
         else if (other.tag == "Customer" && hasPackage)
         {
             Debug.Log("Package delivered");
             hasPackage = false;
+            spriteRenderer.color = noPackageColour;
         }
     }
 }
